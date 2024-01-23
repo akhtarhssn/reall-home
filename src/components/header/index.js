4ec62ba10cdecb76c7ba7b72b8ced484d06d43b0
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-01-24 04:21:27 +0600

    front-end setup

---
+++ src/components/header/index.js
@@ -0,0 +1,5 @@
+import HeaderOne from "./HeaderOne";
+import HeaderTransparent from "./headerTransparent";
+import HeaderTransparentTwo from "./headerTransparentTwo";
+
+export { HeaderOne, HeaderTransparent, HeaderTransparentTwo };
